c9182e490e128f853e67547bf6be9e7a0a0c64a2
vwccompanies23 <vwccompanies23@gmail.com>
2026-07-10 02:21:23 -0400

    Added get user chats endpoint

--- controllers/groups/getGroupsController.js
+++ controllers/groups/getGroupsController.js
@@ -1,35 +1,47 @@
 const pool = require('../../config/db');
 
 const getGroups = async (req, res) => {
+
   try {
 
-   const result = await pool.query(`
-   SELECT
-       g.*,
+    const userId = req.params.userId;
 
-       (
-         SELECT gm.message
-         FROM group_messages gm
-         WHERE gm.group_id = g.id
-         ORDER BY gm.created_at DESC
-         LIMIT 1
-       ) AS last_message,
+    const result = await pool.query(
+      `
+      SELECT
+          g.*,
 
-       (
-         SELECT gm.created_at
-         FROM group_messages gm
-         WHERE gm.group_id = g.id
-         ORDER BY gm.created_at DESC
-         LIMIT 1
-       ) AS last_time
+          (
+            SELECT gm.message
+            FROM group_messages gm
+            WHERE gm.group_id = g.id
+            ORDER BY gm.created_at DESC
+            LIMIT 1
+          ) AS last_message,
 
-   FROM groups g
-   ORDER BY g.created_at DESC
-   `);
+          (
+            SELECT gm.created_at
+            FROM group_messages gm
+            WHERE gm.group_id = g.id
+            ORDER BY gm.created_at DESC
+            LIMIT 1
+          ) AS last_time
+
+      FROM groups g
+
+      INNER JOIN group_members members
+        ON members.group_id = g.id
+
+      WHERE members.user_id = $1
+
+      ORDER BY g.created_at DESC
+      `,
+      [userId],
+    );
 
     res.json({
       success: true,
-      groups: result.rows
+      groups: result.rows,
     });
 
   } catch (error) {
@@ -38,10 +50,11 @@ const getGroups = async (req, res) => {
 
     res.status(500).json({
       success: false,
-      error: error.message
+      error: error.message,
     });
+
   }
+
 };
 
-module.exports =
-getGroups;
+module.exports = getGroups;
