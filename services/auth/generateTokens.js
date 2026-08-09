e320286f212e0f43e3b3480cbd2f530905ba7986
vwccompanies23 <vwccompanies23@gmail.com>
2026-08-09 16:04:06 -0400

    Fix duplicate refresh tokens

--- services/auth/generateTokens.js
+++ services/auth/generateTokens.js
@@ -1,31 +1,52 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { v4: uuid } = require("uuid");
+
+//////////////////////////////////////////////////////
+// ACCESS TOKEN
+//////////////////////////////////////////////////////
 
 function generateAccessToken(user) {
 
     return jwt.sign(
+
         {
             id: user.id,
             username: user.username,
             phone: user.phone,
         },
+
         process.env.JWT_SECRET,
+
         {
             expiresIn: process.env.JWT_EXPIRES,
-        }
+        },
+
     );
 
 }
 
+//////////////////////////////////////////////////////
+// REFRESH TOKEN
+//////////////////////////////////////////////////////
+
 function generateRefreshToken(user) {
 
     return jwt.sign(
+
         {
             id: user.id,
+
+            // Makes every refresh token unique
+            jti: uuid(),
+
         },
+
         process.env.REFRESH_TOKEN_SECRET,
+
         {
             expiresIn: process.env.REFRESH_TOKEN_EXPIRES,
-        }
+        },
+
     );
 
 }
