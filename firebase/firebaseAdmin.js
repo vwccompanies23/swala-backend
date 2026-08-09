ab0545ce0f7d17ecfcbc80df5be0567c8e658788
vwccompanies23 <vwccompanies23@gmail.com>
2026-08-09 02:39:42 -0400

    Replace Firebase Admin SDK key

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./swalaapp-firebase-adminsdk-fbsvc-6793b46e92.json");
+const serviceAccount = require('./swalaapp-firebase-adminsdk-fbsvc-1f9004b198.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
